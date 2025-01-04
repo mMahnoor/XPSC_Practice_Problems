8f7d97a33eecdb8d8b8da7e154b09591378ecc24
mMahnoor <mahnurakther@gmail.com>
2025-01-04 23:54:28 +0600

    week 10 day 07

---
+++ Week10/Day07/E_scuza.cpp
@@ -0,0 +1,46 @@
+// Problem link: https://codeforces.com/problemset/problem/1742/E
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, q;
+        cin >> n >> q;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        map<int, int> k;
+        for (int i = 0; i < q; i++)
+        {
+            int val;
+            cin >> val;
+            k[val]=i;
+        }
+        long long sum=0;
+        int i=0;
+        vector<long long> res(q);
+        for (map<int, int>::iterator it = k.begin(); it != k.end(); it++)
+        {
+            while(i<n && it->first>=a[i])
+            {
+                sum+=a[i];
+                i++;
+            }
+            res[it->second]=sum;
+        }
+        for (int i = 0; i < q; i++)
+        {
+            cout << res[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
