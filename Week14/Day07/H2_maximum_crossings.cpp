da13d9cdfeb25dc89d9f2d01135ac72e5cfb556a
mMahnoor <mahnurakther@gmail.com>
2025-02-05 20:17:09 +0600

    modified

--- Week14/Day07/H2_maximum_crossings.cpp
+++ Week14/Day07/H2_maximum_crossings.cpp
@@ -1,1 +1,34 @@
 // Problem link: https://codeforces.com/problemset/problem/1676/H2
+#include <iostream>
+#include <vector>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        pbds<int> p;
+        long long cnt=0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cnt += p.size()-p.order_of_key(a[i]);
+            p.insert(a[i]);
+        }
+        cout << cnt << "\n";
+    }
+    
+    return 0;
+}
